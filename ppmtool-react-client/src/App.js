d526d57f5f6518d1ce5406a4d95f4963415e920f
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2024-11-05 15:12:19 +0200

    Dashboard & Item components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
