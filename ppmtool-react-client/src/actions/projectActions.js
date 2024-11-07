eccfa626e64a26d14b6a0d7e45bebd4f73f49058
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2024-11-07 22:26:25 +0200

    submit new project form data on server

---
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -0,0 +1,20 @@
+// axios - library for connection with backend
+import axios from "axios";
+import { GET_ERRORS } from "./types";
+
+// async - return a Promise
+export const createProject = (project, navigate) => async (dispatch) => {
+  try {
+    // const navigate = useNavigate();
+    const res = await axios.post("http://localhost:8080/api/project", project);
+    navigate("/dashboard");
+  } catch (err) {
+    console.log(err);
+    console.log(1111111);
+    // validation errors from server, which will be displayed on form
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data,
+    });
+  }
+};
--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
+import { useNavigate } from "react-router-dom";
 
 class AddProject extends Component {
   constructor() {
@@ -29,7 +33,8 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date,
     };
-    console.log(newProject);
+
+    this.props.createProject(newProject, this.props.navigate);
   }
 
   render() {
@@ -108,4 +113,19 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = {};
+
+// Wrapping the main class with this functional component
+function AddProjectFunc(props) {
+  let navigate = useNavigate();
+  return <AddProject {...props} navigate={navigate} />;
+}
+
+export default connect(null, { createProject })(
+  connect(mapStateToProps, mapDispatchToProps)(AddProjectFunc)
+);
