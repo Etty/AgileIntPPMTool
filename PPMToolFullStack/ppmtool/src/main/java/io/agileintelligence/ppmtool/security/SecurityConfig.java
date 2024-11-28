1bb3bfa66b812c85d7b76595f983142bcd5546d5
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2024-11-28 20:36:33 +0100

    Initial Security Config

---
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package io.agileintelligence.ppmtool.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //due to jwt server doesn't need to hold session, redux also responsible for state
+                .and()
+                .headers().frameOptions().sameOrigin() // for h2 db
+                .and()
+                .authorizeRequests()
+                .requestMatchers(
+                        "/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                ).permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
