668d0b4f4363778b3f5eab2f8dd1eef9fb05759d
☆♫~Anastasiia Yermolik~♫☆ <vishnu96vishnu@gmail.com>
2024-12-03 21:43:13 +0100

    Lock operations to specific User (Update)

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -23,6 +23,14 @@ public class ProjectService {
     UserRepository userRepository;
 
     public Project saveOrUpdateProject(Project project, String username) {
+        if (project.getId() != null) {
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if (existingProject!= null && (!existingProject.getProjectLeader().equals(username))) {
+                throw new ProjectIdException("Project not found in your account");
+            } else if(existingProject == null) {
+                throw new ProjectIdException("Project with ID: '" + project.getProjectIdentifier() + "' cannot be updated because it doesn't exist");
+            }
+        }
         try {
             User user = userRepository.findByUsername(username);
             project.setUser(user);
